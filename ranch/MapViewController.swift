47186f368074c9a96cb065b1f5d9a28414b301cb
linux-jedi <calebtho@usc.edu>
2019-01-12 11:14:11 -0800

    refactured mapview and picker to be apart of parent view

--- ranch/MapViewController.swift
+++ ranch/MapViewController.swift
@@ -12,14 +12,20 @@ import GoogleMaps
 class MapViewController: UIViewController, UIPickerViewDelegate,
   UIPickerViewDataSource {
   
-  let pickerData = ["Yosemite", "Yellowstone", "Joshua Tree"]
+  // UIPicker
+  var pickerData: [String] = [String]()
+  var currentParkIndex :Int = 0
   
-  override func loadView() {
+  // Google Map System Components
+  var mapView:GMSMapView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     // Create a GMSCameraPosition that tells the map to display the
     // coordinate -33.86,151.20 at zoom level 6.
     let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
-    let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-    view = mapView
+    mapView = GMSMapView.map(withFrame: view.bounds, camera: camera)
+    view.addSubview(mapView)
     
     // Creates a marker in the center of the map.
     let marker = GMSMarker()
@@ -30,11 +36,13 @@ class MapViewController: UIViewController, UIPickerViewDelegate,
     
     // Initialize Picker
     let parkPicker = UIPickerView()
+    pickerData = ["Yosemite", "Yellowstone", "Joshua Tree"]
     parkPicker.delegate = self
     parkPicker.dataSource = self
-    
+
     // handle picker layout
     self.view.addSubview(parkPicker)
+    self.view.bringSubviewToFront(parkPicker)
   }
   
   //MARK: UIPickerDelegate Mathods
@@ -49,5 +57,16 @@ class MapViewController: UIViewController, UIPickerViewDelegate,
   func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
     return pickerData[row]
   }
+  
+//  func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+//    if row != self.currentParkIndex {
+//      self.currentParkIndex = row
+//      
+//      // move camera to new location
+//      print("New Location: \(pickerData[row])\n")
+//      
+//      
+//    }
+//  }
 }
 
